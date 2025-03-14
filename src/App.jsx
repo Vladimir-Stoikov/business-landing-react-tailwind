7a7e8fde3259f068b2c6d68c54315a11bbf19c07
Vladimir Stoikov <vspsmoke@gmail.com>
2025-03-14 23:13:40 +0300

    style: added assets, change fav icon and title. feat: start create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-      <h1 className='text-4xl font-bold text-blue-500'>Landing App</h1>
+      <Navbar/>
     </>
   )
 }
